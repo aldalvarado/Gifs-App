5bcfa00205180db941846fc280b8fc1bf22ebea4
aldalvarado <aaldoandre@gmail.com>
2024-01-31 14:11:45 -0600

    Service Validations

--- src/app/gifs/services/gifs.service.ts
+++ src/app/gifs/services/gifs.service.ts
@@ -9,8 +9,22 @@ export class GifsService {
     return [...this._TagHistory];
   }
 
-  SearchTag(tag: string) {
+  private OrganizedHistory (tag: string) {
+    tag= tag.toLowerCase();
+
+    if (this._TagHistory.includes(tag)) {
+      this._TagHistory = this._TagHistory.filter((OldTag) => OldTag !== tag)
+    }
+
     this._TagHistory.unshift(tag);
+
+    this._TagHistory = this.TagHistory.splice(0,10);
+
+  }
+
+  SearchTag(tag: string) {
+    if (tag.length === 0) return;
+    this.OrganizedHistory(tag);
   }
 
 }
