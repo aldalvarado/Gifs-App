0e9a47fbb8ce4fb7a1b13434a2240110ef801f56
aldalvarado <aaldoandre@gmail.com>
2024-01-30 21:18:01 -0600

    Diseño Gifs + Tarea

--- src/app/gifs/gifs.module.ts
+++ src/app/gifs/gifs.module.ts
@@ -1,12 +1,22 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { SearchBoxComponent } from './components/Search-Box/Search-BoxComponent';
+import { CardListComponent } from './components/card-list/card-list.component';
 
 
 
 @NgModule({
-  declarations: [],
+  declarations: [
+    HomePageComponent,
+    SearchBoxComponent,
+    CardListComponent
+  ],
   imports: [
     CommonModule
+  ],
+  exports: [
+    HomePageComponent
   ]
 })
 export class GifsModule { }
